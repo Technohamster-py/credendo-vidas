62e5c455a216358370a956182a495f6b10dd2190
Technohamster <arseniy.khomchenko@gmail.com>
2024-04-17 15:30:47 +0300

    Добавлено поле "краткое содержание"

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,13 @@ from cuser.fields import CurrentUserField
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    title = models.CharField('Заголовок', max_length=150)
+    title = models.CharField('Заголовок', max_length=50)
     body = models.TextField('Тело статьи')
     date = models.DateTimeField('Дата создания', auto_now_add=True)
     #date_published = models.DateTimeField('Дата публикации', blank=True, null=True)
     author = CurrentUserField(related_name='post_author', add_only=True, on_delete=models.CASCADE, verbose_name='Автор')
     cover = models.ImageField('Обложка', blank=True)
+    summary = models.CharField('Краткое описание', max_length=200, blank=True)
     is_published = models.BooleanField('Опубликовать', default=True)
 
     class Meta:
